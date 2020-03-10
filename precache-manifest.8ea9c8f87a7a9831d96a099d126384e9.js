68042090a1b4f772c01e7be9ecc12109a62c9653
Jacky Wang <trac@localhost>
2020-03-10 12:46:41 +0800

    first commit

---
+++ precache-manifest.8ea9c8f87a7a9831d96a099d126384e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89baa91c63d38a90a75691880306435d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eec3ad4ee8c8a42b760",
+    "url": "/static/css/main.46c5fc3b.chunk.css"
+  },
+  {
+    "revision": "112844385b4fe182f393",
+    "url": "/static/js/2.df7cb656.chunk.js"
+  },
+  {
+    "revision": "b0be6c033a4a708ceaccf5eb2843e54b",
+    "url": "/static/js/2.df7cb656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eec3ad4ee8c8a42b760",
+    "url": "/static/js/main.92da3fba.chunk.js"
+  },
+  {
+    "revision": "151ea5f2f609c6413480",
+    "url": "/static/js/runtime-main.a0a8f0a2.js"
+  },
+  {
+    "revision": "28268cc187666021f6b05eb8dfbfb781",
+    "url": "/static/media/1.28268cc1.png"
+  }
+]);
